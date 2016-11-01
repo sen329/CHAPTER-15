6760ca5b97d8b66657a0d503bf9a6636b182bb02
sen329 <hans.hilman@gmail.com>
2016-11-01 10:18:05 +0700

    NOMOR 1, 2, 3

--- team leader.cpp
+++ team leader.cpp
@@ -50,14 +50,11 @@ void Employee::setdate(string date){
 } 
 
 class ProductionWorker:public Employee{
-private:
+protected:
 	int shift;
 	double hourlypayrate;
 public: 
-	ProductionWorker(int shift, double hourlypayrate){
-		this -> shift = shift;
-		this -> hourlypayrate = hourlypayrate;
-	};
+	ProductionWorker(int shift, double hourlypayrate);
 	void setshift(int);
 	int getshift();
 	void sethourlypayrate(double);
@@ -65,6 +62,10 @@ public:
 	void print();
 };
 
+ProductionWorker::ProductionWorker(int shift, double hourlypayrate){
+	this -> shift = shift;
+	this -> hourlypayrate = hourlypayrate;
+}
 
 void ProductionWorker::setshift(int shift){
 	this -> shift = shift;
@@ -90,23 +91,21 @@ void ProductionWorker::print(){
 	
 	if (shift == 1){
 		cout << "Day shift"<<endl;
-		cout << "Pay rate: $"<< gethourlypayrate()<<endl<<endl;
+		cout << "Pay rate: $"<< gethourlypayrate()<<endl;
 	}
 	else if(shift == 2){
 		cout << "Night shift"<<endl;
-		cout << "Pay rate: $"<< gethourlypayrate()<<endl<<endl;
+		cout << "Pay rate: $"<< gethourlypayrate()<<endl;
 	}
 	else {
 		cout << "Invalid shift"<<endl;
 	}
 }
 
-
 class ShiftSupervisor:public Employee{
 private:
 	int AnnualSalary;
 	int AnnualProductionBonus;
-	string supername;
 public: 
 	ShiftSupervisor(int AnnualSalary, int AnnualProductionBonus){
 		this -> AnnualSalary = AnnualSalary;
@@ -116,8 +115,6 @@ public:
 	void setAnnualSalary(int);
 	int getAnnualProductionBonus();
 	void setAnnualProductionBonus(int);
-	string getsupername();
-	void setsupername(string);
 	void print2();
 };
 
@@ -137,19 +134,74 @@ int ShiftSupervisor::getAnnualProductionBonus(){
 	return AnnualProductionBonus;
 }
 
-string ShiftSupervisor::getsupername(){
-	return supername;
-}
-
-void ShiftSupervisor::setsupername(string supername){
-	this -> supername = supername;
-}
-
 void ShiftSupervisor::print2(){
 	cout << "Shift Supervisor salary"<<endl;
-	cout << "Shift Supervisor: "<<getsupername()<<endl;
 	cout << "Annual Salary: $"<<getAnnualSalary()<<endl;
-	cout << "Annual Production Bonus: $" <<getAnnualProductionBonus()<<endl; 
+	cout << "Annual Production Bonus: $" <<getAnnualProductionBonus()<<endl<<endl; 
+}
+
+class TeamLeader:public ProductionWorker{
+private:
+	int monthlybonus;
+	int reqtrainhours;
+	int trainhoursattend;
+	string teamname;
+public:
+	TeamLeader(int reqtrainhours, int trainhoursattend, int monthlybonus):ProductionWorker(shift, hourlypayrate)
+	{
+		this -> reqtrainhours = reqtrainhours;
+		this -> trainhoursattend = trainhoursattend;
+		this -> monthlybonus = monthlybonus;
+	};
+	string getteamname();
+	void setteamname(string);
+	int getmonthlybonus();
+	void setmonthlybonus(int);
+	int getreqtrainhours();
+	void setreqtrainhours(int);
+	int gettrainhoursattend();
+	void settrainhoursattend(int);
+	void print3();
+};
+
+
+string TeamLeader::getteamname(){
+	return teamname;
+}
+
+void TeamLeader::setteamname(string teamname){
+	this -> teamname = teamname;
+}
+
+int TeamLeader::getreqtrainhours(){
+	return reqtrainhours;
+}
+
+int TeamLeader::getmonthlybonus(){
+	return monthlybonus;
+}
+
+int TeamLeader::gettrainhoursattend(){
+	return trainhoursattend;
+}
+
+void TeamLeader::setreqtrainhours(int reqtrainhours){
+	this -> reqtrainhours = reqtrainhours;
+}
+
+void TeamLeader::setmonthlybonus(int monthlybonus){
+	this -> monthlybonus = monthlybonus;
+}
+
+void TeamLeader::settrainhoursattend(int trainhoursattend){
+	this -> trainhoursattend = trainhoursattend;
+}
+
+void TeamLeader::print3(){
+	cout << "Team leader name: "<<getteamname()<<endl;
+	cout << "Monthly bonus: $"<<getmonthlybonus()<<endl;
+	cout << "Training hours needed: "<< getreqtrainhours()<< endl;
+	cout << "Training hours attended: " << gettrainhoursattend()<<endl; 
 }
 
 int main(){
@@ -157,6 +209,9 @@ int main(){
 	double pay;
 	int AnnualSalary;
 	int AnnualProductionBonus;
+	int monthlybonus;
+	int reqtrainhours;
+	int trainhoursattend;
 	cout << "Enter shift 1 for day shift or 2 for night shift: "<<endl;
 	cin >> shift;
 	cout << "Enter hourly pay: ";
@@ -169,10 +224,15 @@ int main(){
 	empl1.print();
 	
 	ShiftSupervisor emplAS1(AnnualSalary, AnnualProductionBonus);
-	emplAS1.setsupername("JOKO");
 	emplAS1.setAnnualSalary(500);
 	emplAS1.setAnnualProductionBonus(100);
 	emplAS1.print2();
+	
+	TeamLeader emplTL1(reqtrainhours, trainhoursattend, monthlybonus);
+	emplTL1.setmonthlybonus(100);
+	emplTL1.setteamname("BUDI");
+	emplTL1.settrainhoursattend(6);
+	emplTL1.setreqtrainhours(12);
 	return 0;
 }
 
